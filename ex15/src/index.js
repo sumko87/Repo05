8cf9ed7b626f24efda88cc43c81cbf46d64184a8
Sumedin Bilalic <sumko87@gmail.com>
2022-04-01 23:17:22 +0200

    playing with word blanks

---
+++ ex15/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    //Only change code below this line
+    var myNoun="dog";
+    var myVerb="run";
+    var myAdjective="big";
+    var myAdverb="quickly";
+    var wordBlanks="I have a " + myNoun +" which is " + myAdjective + " and can " + myVerb + " very " + myAdverb +"."
+    //Only change code above this line
+    return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;
