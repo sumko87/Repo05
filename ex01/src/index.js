3de7694d11577aef750cdf2e19d68c181803224a
Sumedin Bilalic <sumko87@gmail.com>
2022-04-01 20:36:40 +0200

    Started to work with string variables

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    //Only change code below this line
+    var myFirstName="Sumedin";
+    var myLastName="Bilalic";
+    var myHomeTown="Sarajevo";
+    //Only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown,
+    };
+}
+
+console.log(main());
+module.exports = main;
