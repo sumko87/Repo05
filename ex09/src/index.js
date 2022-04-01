e3475e7561b6ed6a422ee95cc36b40027872e71a
Sumedin Bilalic <sumko87@gmail.com>
2022-04-01 22:24:12 +0200

    finding a length of a string

---
+++ ex09/src/index.js
@@ -0,0 +1,10 @@
+function main(){
+    //Only change code below this line
+    var lastname="Doe";
+    var lastNameLength=lastname.length;
+    //Only change code above this line
+    return lastNameLength;
+}
+
+console.log(main());
+module.exports = main;
